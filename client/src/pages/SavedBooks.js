9ba23427ce098dbb4e2a706db02df1709264b911
ForestW70 <hexaforest@gmail.com>
2021-07-25 16:02:27 -0400

    set pages and components to import queries and mutations rather than API calls.

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { useMutation, useQuery } from '@apollo/client';
 
-import { getMe, deleteBook } from '../utils/API';
+
+// import { getMe, deleteBook } from '../utils/API';
+import { REMOVE_BOOK } from '../utils/mutations';
+import { QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
