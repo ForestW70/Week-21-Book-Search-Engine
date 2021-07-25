c1a15b8915e0977d60c66780fad451a8311c8923
ForestW70 <hexaforest@gmail.com>
2021-07-25 15:35:30 -0400

    added queries and mutation control on the client side in utils.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+    mutation addUser(
+        $username: String!,
+        $email: String!,
+        $password: String!
+        ) {
+            addUser(
+                username: $username,
+                email: $email,
+                password: $password
+            ) {
+                token
+                user {
+                    _id
+                    username
+                }
+            }
+        }
+`;
+
+export const LOGIN_USER = gql`
+    mutation login(
+        $email: String!,
+        $password: String!
+    ) {
+        login(
+            email: $email,
+            password: $password
+        ) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: BookId!) {
+        saveBook(book: $book) {
+            username
+            savedBooks {
+                bookId
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            username
+            savedBooks {
+                bookId
+                image
+                authors
+                title
+                description
+            }
+
+        }
+    }
+`;
